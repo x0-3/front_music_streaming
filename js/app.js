e3c905aa0891c36438b59215ba245261b4ddf77d
x0-3 <steph6363@gmail.com>
2023-04-29 19:31:36 +0200

    hamburger menu finished

--- js/app.js
+++ js/app.js
@@ -3,6 +3,19 @@ function myFunction() {
   element.classList.toggle("dark");
 }
 
+
+// ********************************************** Hamburger menu ****************************************************************** * // 
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+
 // *********************************************************************************************************** * //
 // audio player
 
