3e548b958c0c31c79ac89fa386bb78e479a9bf7e
x0-3 <steph6363@gmail.com>
2023-05-02 09:52:03 +0200

    add dinamique volume logo, link for a song

--- js/app.js
+++ js/app.js
@@ -42,9 +42,9 @@ const playBtn = document.querySelector("#mainPlayBtn");
 const audio = document.querySelector("#audio");
 const btnPrev = document.querySelector("#btnPrev");
 const btnNext = document.querySelector("#btnNext");
-const trackTitle = document.querySelector(".trackTitle");
-const ArtistName = document.querySelector(".ArtistName");
-const cover = document.querySelector(".cover");
+// const trackTitle = document.querySelector(".trackTitle");
+// const ArtistName = document.querySelector(".ArtistName");
+// const cover = document.querySelector(".cover");
 const slider = document.querySelector(".slider");
 const thumb = document.querySelector(".slider-thumb");
 const progress = document.querySelector(".progress");
@@ -52,19 +52,20 @@ const time = document.querySelector(".time");
 const fulltime = document.querySelector(".fulltime");
 const volumeSlider = document.querySelector(".volume-slider .slider");
 const volumeProgress = document.querySelector(".volume-slider .progress");
-const volumeIcon = document.querySelector("volume-icon");
+const volumeIcon = document.querySelector(".volume-icon");
 
 
 let trackPlaying = false;
 
-let volumeMuted = false;
+// let volumeMuted = false;
 
 let trackId = 0; 
 
 // TODO: make it dinamic with AJAX
 const tracks = [
-  "Aaliyah - Giving You More.mp3",
-  "Aaliyah - Giving You More.mp3"
+  "Aaliyah - Giving You More",
+  "Ella Mai - Good Bad",
+  "Aaliyah - Giving You More"
 ];
 
 // play /pause 
@@ -72,51 +73,69 @@ playBtn.addEventListener("click", playTrack);
 
 function playTrack(){
 
+  // if the track is not playing
   if(trackPlaying === false){
 
+    // play the audio
     audio.play();
 
+    // change the button with a pause icon
     playBtn.innerHTML = `
     <span class="fa-solid fa-pause"></span>
     `;
 
+    // set it to true
     trackPlaying = true;
-  }else{
+  }else{ //else if the track is already playing then
 
+    // stop the audio
     audio.pause();
 
+    // change the button with a play icon
     playBtn.innerHTML = ` <span class="fa-solid fa-circle-play"></span> `;
 
+    // set it to false
     trackPlaying = false;
 
   }
 }
 
-
+// switch songs
 function switchtrack(){
 
+  // if the audio is playing 
   if(trackPlaying === true){
 
+    // then keep it playing
     audio.play();
 
   }
 }
 
-const trackScr = 'music/' + tracks[trackId] + "mp3";
+// get track source
+const trackScr = "assets/music/" + tracks[trackId] + ".mp3";
 
 function loadTrack(){
 
-  audio.scr = 'music/' + tracks[trackId] + "mp3";
+  // set the audio track source
+  audio.scr = "assets/music/" + tracks[trackId] + ".mp3";
 
+  // console.log(audio);
+
+  // reload the track
   audio.load();
+
 
   progress.style.width = 0;
   thumb.style.left = 0;
 
+  // wait for the song to load
   audio.addEventListener('loadedData', () => {
 
+    // display the duration of the song
     setTime(fulltime, audio.duration);
 
+    // set the max value to the slider
     slider.setAttribute("max", audio.duration);
   });
 }
@@ -128,8 +147,10 @@ btnPrev.addEventListener('click', () => {
 
   trackId--;
 
+  // if the track id is negative  
   if (trackId < 0) {
-    
+
+    // go back to the last song
     trackId = tracks.length - 1;
   }
 
@@ -138,7 +159,6 @@ btnPrev.addEventListener('click', () => {
   switchtrack();
 
 });
-
 
 // switch to next song
 btnNext.addEventListener("click", nextTrack);
@@ -149,6 +169,7 @@ function nextTrack(){
 
   if (trackId > tracks.length - 1) {
 
+    // set the id to 0
     trackId = 0;
   }
     
@@ -164,15 +185,21 @@ audio.addEventListener('ended', nextTrack);
 // music time 
 function setTime(output, input){
 
+  // calculate the minutes
   const minutes = Math.floor(input / 60);
   
+  // calculate the seconds
   const seconds = Math.floor(input % 60);
 
+  // if seconds is under 10
   if (seconds < 10) {
     
+    // show a zero 
     output.innerHTML = minutes + ":0" + seconds;
 
   }else{
+
+    // show the :
     output.innerHTML = minutes + ":" + seconds;
   }
 }
@@ -181,14 +208,18 @@ function setTime(output, input){
 setTime(fulltime, audio.duration);
 
 
-audio.addEventListener("timeupdate", () => {
+audio.addEventListener('timeupdate', () => {
 
+  // get the current audio time
   const currentAudioTime = Math.floor(audio.currentTime);
   
+  // get the percentage
   const timePercentage = (currentAudioTime / audio.duration) * 100 + "%";
 
+  // set the current time of the song
   setTime(time, currentAudioTime);
 
+  // set the slider to the time
   progress.style.width = timePercentage;
   thumb.style.left = timePercentage;
 
@@ -198,13 +229,19 @@ audio.addEventListener("timeupdate", () => {
 // music slider
 function customSlider(){
 
+  // calculate the % 
   const val = (slider.value / audio.duration) * 100 + "%";
 
+  // console.log(audio.duration);
+
+  // set the progress of the song
   progress.style.width = val;
   thumb.style.left = val;
 
+  // show the cuurent time of the song
   setTime(time, slider.value);
 
+  // set it to the slider 
   audio.currentTime = slider.value;
 }
 
@@ -217,5 +254,22 @@ slider.addEventListener("input", customSlider);
 let volume = document.getElementById('volume-slider');
 volume.addEventListener("change", function(e) {
   audio.volume = e.currentTarget.value / 100;
-})
 
+  // if the volume is high
+  if (audio.volume > 0.5) {
+  
+    // change the icon 
+    volumeIcon.innerHTML = `<i class="fa-solid fa-volume-high"></i>`;
+
+  } else if (audio.volume === 0){
+
+    // mute icon
+    volumeIcon.innerHTML = `<i class="fa-solid fa-volume-xmark"></i>`;
+
+  } else{
+
+    // volume low
+    volumeIcon.innerHTML = `<i class="fa-solid fa-volume-low"></i>`;
+
+  }
+});
